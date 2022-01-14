4f2f15327ea89b025b7ccf99d51fc6295b198ad3
mjdcruz <84747487+mjdcruz@users.noreply.github.com>
2022-01-13 22:20:54 -0800

    UI Prototype Testing
    Updated code to include a working navigation bar. Note: updates only provides navigation between pages via the navbar, still no functionality with buttons and text in pages

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
-
+@app.route('/order/')
+def order():
+    return render_template('order.html')
+@app.route('/account/')
+def account():
+    return render_template('account.html')
 if __name__ == "__main__":
     app.run(debug=True)

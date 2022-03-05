758ba77b2a36847def78366a26d772e8ae61eb68
JJSteiner <82476553+JJSteiner@users.noreply.github.com>
2022-03-05 13:07:15 -0800

    Add files via upload
    working on getting multiple locations, but this is the base of the map

---
+++ legitmap.js
@@ -0,0 +1,44 @@
+// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&libraries=places">
+let map;
+let service;
+let infowindow;
+
+function initMap() {
+  const ellensburg = new google.maps.LatLng(46.9965, 120.5478);
+
+  infowindow = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: ellensburg,
+    zoom: 15,
+  });
+
+  const request = {
+    query: "Starbucks",
+    fields: ["name", "geometry"],
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+}

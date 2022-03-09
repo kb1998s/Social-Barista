881c15e231f34b44e24b79d320eabb66caa9caab
mjdcruz <84747487+mjdcruz@users.noreply.github.com>
2022-03-08 17:11:30 -0800

    Map Integration

---
+++ src/static/js/map.js
@@ -0,0 +1,43 @@
+let map;
+let service;
+let infowindow;
+
+function initMap() {
+  const ellensburg = new google.maps.LatLng(46.9965, 120.5478);
+
+  infowindow = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: ellensburg,
+    zoom: 15,
+  });
+
+  const request = {
+    query: "Starbucks",
+    fields: ["name", "geometry"],
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+}
